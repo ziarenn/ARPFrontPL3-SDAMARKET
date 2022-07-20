8b6522641847838344be4968b72447f1f80d9188
Kacper Żarna <kacper-zarna@wp.pl>
2022-07-20 18:38:06 +0200

    Kod 20.07.2022 18:40

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -44,3 +44,33 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+
+
+
+// interface Product
+// interface Rating
+
+// Stwórz komponent Bestsellers
+// stan bestsellers (typ Product[])
+// useEffect i fetch do linku `https://fakestoreapi.com/products?limit=10`, response do stanu bestsellers
+// JSX: 
+// Card
+// w Card pętla z wyświetleniem komponentów ProductTile
+
+// interface ProductTileProps, product: Product
+
+// Stwórz komponent ProductTile, propsy: product
+// stan clicked
+// JSX:
+// Card (onClick przestawia stan clicked na true jeżeli jest false i na odwrót):
+//--------
+// Box img (src=product.image)
+// Typography z product.title
+// Typography z product.category
+// Typography z product.price
+// Typography z product.rating.rate i product.rating.count
+// --------
+// Warunkowo renderowany Card w zależności od stanu clicked {clicked && <Card ...}:
+// Typography z product.description
+// Button Add to cart
